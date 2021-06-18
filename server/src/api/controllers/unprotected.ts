063eb279727ead8e6fe8cdebe25bc6b34d49db0a
Matej Kriš <3matej.k3@gmail.com>
2021-06-18 10:25:55 +0200

    Add login endpoint with json web token creation

--- server/src/api/controllers/unprotected.ts
+++ server/src/api/controllers/unprotected.ts
@@ -2,6 +2,7 @@ import { Response, Request } from 'express';
 import { Users } from '../../entities/users';
 import bcrypt from 'bcrypt';
 import { logger } from '../../config/logger';
+import jwt from "jsonwebtoken";
 
 export const default_page = async (req: Request, res: Response) => {
     res.status(200).send({ message: "Welcome!"});
@@ -10,9 +11,23 @@ export const default_page = async (req: Request, res: Response) => {
 export const userLogin = async (req: Request, res: Response) => {
     const { email , password } = req.body;
 
-    const account = await Users.findOne({ email });
+    try {
+        const account = await Users.findOne({ email });
 
-    res.status(200).send({ message: "Login!"});
+        if (!account || !bcrypt.compareSync(password, account.password)) {
+            res.status(401).send({ message: "Wrong email or password" });
+        } else {
+            let token: string = jwt.sign( { email: email }, "secret123", {
+                expiresIn: 86400 // expires in 24 hours
+              });
+              
+            res.status(200).send({ token: token });
+        }   
+        
+    } catch (e) {
+        logger.error(e.stack);
+        res.status(500).send({ message: "Something went wrong" });
+    }
 };
 
 export const userRegister = async (req: Request, res: Response) => {
