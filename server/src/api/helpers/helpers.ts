8fe8b0cb975bc4eb20bc46320834a35239a5d01d
Matej Kriš <3matej.k3@gmail.com>
2021-06-19 16:16:19 +0200

    Add jwt validation for protected endpoints

--- server/src/api/helpers/helpers.ts
+++ server/src/api/helpers/helpers.ts
@@ -1,7 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
 
 export const invalidRequest = (req: Request, res: Response) => {
     res.status(404).send({ message: "Invalid request"}); 
 };
+
+export const jwtValidation = (res: Response, next: NextFunction, token: string) => {
+    try {
+        jwt.verify(token, 'secret123');
+        next();
+    } catch (e) {
+        res.status(500).send({ message: "Invalid token" });
+    }
+}
 
 
--- server/src/api/validators/jwt.ts
+++ server/src/api/validators/jwt.ts
@@ -1,7 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
-
+import { jwtValidation } from '../helpers/helpers';
 
 export const validateJwt = (req: Request, res: Response, next: NextFunction) => {
-    console.log("Valid");
-    next();
-}
+    let authHeader: string = req.headers.authorization!;
+
+    try {
+        if (authHeader.startsWith('Bearer')) {
+                const token: string = authHeader.split(' ')[1];
+                jwtValidation(res, next, token);
+        } else {
+            res.status(401).send({ message: "Authorization not supported" });
+        }
+    } catch (e) {
+        res.status(500).send({ message: "Something went wrong" });
+    }
+} 
